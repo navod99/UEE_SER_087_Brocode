1098568aff60bc86d7f29654f3db74d6a00d01a2
navod99 <navx99@gmail.com>
2022-11-10 02:07:26 +0530

    home drawer

--- EcoFirst/app/Tabs/Home.js
+++ EcoFirst/app/Tabs/Home.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import { Text, View,StyleSheet } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { Request } from './Request'
 import { Discussions } from './Discussions'
 export const Home = () => {
   const Drawer = createDrawerNavigator();
+
   return (
-    
-       <>
+    <>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Request}  />
-    <Drawer.Screen name="Notifications" component={ Discussions} />
+        <Drawer.Screen name="Events" component={Request} />
+        <Drawer.Screen name="Clubs" component={Discussions} />
+        <Drawer.Screen name="Professionals" component={Discussions} />
       </Drawer.Navigator>
     </>
-      
-     
+
   )
 }
 const styles = StyleSheet.create({
